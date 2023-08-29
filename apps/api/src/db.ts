cf360875cbd8923bd61ab7793d4da196058b4212
eplougoulm <eplougoulm@lemonway.com>
2023-08-29 19:49:08 +0200

    [MOULA-MANAGER]: add config

--- apps/api/src/db.ts
+++ apps/api/src/db.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 import config from 'config';
-
 import { logger } from './utils/logger';
 
 function dbConnection() {
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,25 @@
+import config from 'config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { Transport } from '@nestjs/microservices';
+import { dbConnection } from './db';
 
 async function bootstrap() {
+  // const { hostname, port }: any = config.get('api');
+
+  // const appConfig = {
+  //   port,
+  //   host: hostname,
+  //   transport: Transport.RMQ,
+  //   options: { ...options, urls: brokerUrls },
+  // };
+
+  // const { npm_package_name: name, npm_package_version: version } = process.env;
+
+  await dbConnection();
+
   const app = await NestFactory.create(AppModule);
+
   await app.listen(3005);
 }
 bootstrap();
