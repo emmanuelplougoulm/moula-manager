28526734c8299ee93dbdb6922e33131d06010822
e.plougoulm <e.plougoulm@groupeonepoint>
2023-11-13 11:59:45 +0100

    [MOULA-MANAGER]: setup Eslint & Prettier

--- apps/web/components/card-transaction/card-transaction.vue
+++ apps/web/components/card-transaction/card-transaction.vue
@@ -2,13 +2,7 @@
   <form action="" method="post" class="form-example">
     <div class="form-example">
       <label for="type">transaction type </label>
-      <input
-        type="text"
-        v-model="transactionState.type"
-        name="type"
-        id="type"
-        required
-      />
+      <input type="text" v-model="transactionState.type" name="type" id="type" required />
     </div>
     <div class="form-example">
       <label for="wallet-name">wallet-name </label>
@@ -22,13 +16,7 @@
     </div>
     <div class="form-example">
       <label for="date">date </label>
-      <input
-        type="text"
-        v-model="transactionState.date"
-        name="date"
-        id="date"
-        required
-      />
+      <input type="text" v-model="transactionState.date" name="date" id="date" required />
     </div>
     <div class="form-example">
       <label for="currency">currency </label>
@@ -42,43 +30,19 @@
     </div>
     <div class="form-example">
       <label for="symbol">symbol </label>
-      <input
-        type="text"
-        v-model="transactionState.symbol"
-        name="symbol"
-        id="symbol"
-        required
-      />
+      <input type="text" v-model="transactionState.symbol" name="symbol" id="symbol" required />
     </div>
     <div class="form-example">
       <label for="amount">amount </label>
-      <input
-        type="text"
-        v-model="transactionState.amount"
-        name="amount"
-        id="amount"
-        required
-      />
+      <input type="text" v-model="transactionState.amount" name="amount" id="amount" required />
     </div>
     <div class="form-example">
       <label for="price">price </label>
-      <input
-        type="text"
-        v-model="transactionState.price"
-        name="price"
-        id="price"
-        required
-      />
+      <input type="text" v-model="transactionState.price" name="price" id="price" required />
     </div>
     <div class="form-example">
       <label for="fees">fees </label>
-      <input
-        type="text"
-        v-model="transactionState.fees"
-        name="fees"
-        id="fees"
-        required
-      />
+      <input type="text" v-model="transactionState.fees" name="fees" id="fees" required />
     </div>
 
     <div class="form-example">
@@ -88,22 +52,23 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
-import httpClient from "../../services/httpClient/httpClient";
-import {createTransaction} from "../../services/transactions/transactions.service"
+import { reactive } from 'vue';
+import httpClient from '../../services/httpClient/httpClient';
+import { createTransaction } from '../../services/transactions/transactions.service';
 
 const transactionState = reactive({
   type: null,
-  walletName: "",
-  date: "",
-  currency: "",
-  symbol: "",
+
+  walletName: '',
+  date: '',
+  currency: '',
+  symbol: '',
   amount: 0,
   price: 0,
-  fees: 0,
+  fees: 0
 });
 
 const handleSubmit = () => {
-  createTransaction(transactionState)
+  createTransaction(transactionState);
 };
 </script>
