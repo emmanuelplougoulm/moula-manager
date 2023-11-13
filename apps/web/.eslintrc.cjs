91ae841540168b1e5cb1ae3b950ae605f9faa51d
e.plougoulm <e.plougoulm@groupeonepoint>
2023-11-13 18:35:58 +0100

    [MOULA-MANAGER]: update ESlint setup

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ['custom', 'plugin:vue/vue3-strongly-recommended'],
+  rules: {
+    // 'vue/max-attributes-per-line': 'off',
+    // 'vue/html-self-closing': 'off'
+  }
+};
