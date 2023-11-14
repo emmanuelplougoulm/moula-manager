57edd2843aba88764312b561988079cc19feb557
e.plougoulm <e.plougoulm@groupeonepoint>
2023-11-14 16:16:43 +0100

    [M-M]: ModalTransaction > add CSS structure

--- apps/web/components/card-transaction/card-transaction.vue
+++ apps/web/components/card-transaction/card-transaction.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
-import httpClient from '../../services/httpClient/httpClient';
+import { reactive, computed } from 'vue';
+// import httpClient from '../../services/httpClient/httpClient';
 import { createTransaction } from '../../services/transactions/transactions.service';
 
 const transactionState = reactive({
-  type: null,
+  type: '',
   walletName: '',
   date: '',
   currency: '',
   symbol: '',
-  amount: 0,
+  quantity: 0,
   price: 0,
   fees: 0
 });
+
+const total = computed(() => 'toto');
 
 const handleSubmit = () => {
   createTransaction(transactionState);
@@ -21,51 +23,48 @@ const handleSubmit = () => {
 
 <template>
   <form action="" method="post" class="form-example card-transaction">
-    <div class="form-example">
-      <label for="type">transaction type </label>
-      <input type="text" v-model="transactionState.type" name="type" id="type" required />
+    <div class="transaction-type">
+      <ul class="buy-or-sell">
+        <li><div @click="transactionState.type = 'BUY'">BUY</div></li>
+        <li><div @click="transactionState.type = 'BUY'">SELL</div></li>
+      </ul>
     </div>
-    <div class="form-example">
-      <label for="wallet-name">wallet-name </label>
-      <input
-        type="text"
-        v-model="transactionState.walletName"
-        name="wallet-name"
-        id="wallet-name"
-        required
-      />
+    <div class="select-coin">
+      <select>
+        <option value="BTC">$BTC</option>
+        <option value="ETH">$ETH</option>
+        <option value="TAO">$TAO</option>
+      </select>
     </div>
-    <div class="form-example">
-      <label for="date">date </label>
-      <input type="text" v-model="transactionState.date" name="date" id="date" required />
+    <div class="quantity-and-price">
+      <div class="quantity">
+        <label for="quantity">quantity </label>
+        <input
+          type="text"
+          v-model="transactionState.quantity"
+          name="quantity"
+          id="quantity"
+          required
+        />
+      </div>
+      <div class="price">
+        <label for="price">price </label>
+        <input type="text" v-model="transactionState.price" name="price" id="price" required />
+      </div>
     </div>
-    <div class="form-example">
-      <label for="currency">currency </label>
-      <input
-        type="text"
-        v-model="transactionState.currency"
-        name="currency"
-        id="currency"
-        required
-      />
+    <div class="date-and-fees">
+      <div class="date">
+        <label for="date">date</label>
+        <input type="text" v-model="transactionState.date" name="date" id="date" required />
+      </div>
+      <div class="fees">
+        <label for="fees">fees</label>
+        <input type="text" v-model="transactionState.fees" name="fees" id="fees" required />
+      </div>
     </div>
-    <div class="form-example">
-      <label for="symbol">symbol </label>
-      <input type="text" v-model="transactionState.symbol" name="symbol" id="symbol" required />
+    <div class="total-spent">
+      <div name="total">{{ total }}</div>
     </div>
-    <div class="form-example">
-      <label for="amount">amount </label>
-      <input type="text" v-model="transactionState.amount" name="amount" id="amount" required />
-    </div>
-    <div class="form-example">
-      <label for="price">price </label>
-      <input type="text" v-model="transactionState.price" name="price" id="price" required />
-    </div>
-    <div class="form-example">
-      <label for="fees">fees </label>
-      <input type="text" v-model="transactionState.fees" name="fees" id="fees" required />
-    </div>
-
     <div class="form-example">
       <input type="submit" value="+ Add transaction" @click="handleSubmit" />
     </div>
@@ -73,7 +72,64 @@ const handleSubmit = () => {
 </template>
 
 <style scoped>
+.form-example {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
 .card-transaction {
   border: 1px green solid;
+  background-color: cadetblue;
+}
+
+.buy-or-sell {
+  display: flex;
+  flex-direction: row;
+  justify-content: baseline;
+  background-color: lightskyblue;
+  padding: 10px;
+  border-radius: 4px;
+}
+.buy-or-sell li {
+  border: 1px red solid;
+  flex: 1;
+  list-style: none;
+  margin: 5px;
+  border-radius: 4px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+}
+.select-coin {
+  width: 100%;
+  border-radius: 4px;
+}
+.select-coin select {
+  width: 100%;
+  height: 50px;
+  background-color: lightskyblue;
+}
+
+.quantity-and-price,
+.date-and-fees {
+  display: flex;
+  justify-content: center;
+}
+
+.quantity,
+.price,
+.date,
+.fees {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.total-spent {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  height: 80px;
+  background-color: lightskyblue;
 }
 </style>
--- apps/web/components/ui/modal/modal.vue
+++ apps/web/components/ui/modal/modal.vue
@@ -42,7 +42,7 @@
 }
 
 .modal-container {
-  width: 300px;
+  width: 450px;
   margin: 0px auto;
   padding: 20px 30px;
   background-color: #fff;
