b41cb557a5f8ee30e6e8f5d7203043e5ca953ce2
e.plougoulm <e.plougoulm@groupeonepoint>
2023-11-20 16:22:31 +0100

    [M-M]: add getAllTransactions service

--- apps/web/services/transactions/transactions.service.ts
+++ apps/web/services/transactions/transactions.service.ts
@@ -3,10 +3,21 @@ import httpClient from "../httpClient/httpClient";
 export async function createTransaction(input: object) {
     try {
         console.log('input', input)
-        const response = await httpClient.post("/transactions", input.value);
-        return response.data
+        return await httpClient.post("/transactions", input.value);
+
     } catch (error) {
         console.error('Error in POST request:', error);
+        throw error;
+    }
+}
+
+export async function getAllTransactions() {
+    try {
+        const transactions = await httpClient.get("/transactions");
+        console.log("/transactions", transactions.data)
+        return transactions
+    } catch (error) {
+        console.error('Error in GET ALL request:', error);
         throw error;
     }
 }
