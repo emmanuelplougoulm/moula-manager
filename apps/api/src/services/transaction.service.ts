8f7cab8152e940a6c5409fb781bb3e969486740c
e.plougoulm <e.plougoulm@groupeonepoint>
2023-11-21 16:53:32 +0100

    [M-M]: remove getAllTransactions useless method

--- apps/api/src/services/transaction.service.ts
+++ apps/api/src/services/transaction.service.ts
@@ -3,16 +3,6 @@ import Transaction from '../models/transaction';
 
 @Injectable()
 export class TransactionService {
-    async getAllTransactions(): Promise<any> {
-        try {
-            const transactions = await Transaction.find()
-            console.log('transactions', transactions)
-            return { result: { transactions } }
-        } catch (err) {
-            console.log('err', err)
-            throw err
-        }
-    }
     async createOneTransaction(transactionData: any): Promise<object> {
         try {
             const transaction = await new Transaction(transactionData).save();
