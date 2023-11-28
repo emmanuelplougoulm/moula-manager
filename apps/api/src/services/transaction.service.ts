a14c4de679a61c10ea35ca4c8ff13c5cdf939000
e.plougoulm <emmanuel.plougoulm@gmail.com>
2023-11-28 15:48:24 +0100

    [MM-API]: simplify createTransaction method

--- apps/api/src/services/transaction.service.ts
+++ apps/api/src/services/transaction.service.ts
@@ -8,30 +8,15 @@ import { v4 as uuidv4 } from 'uuid';
 export class TransactionService {
     async createOneTransaction(transactionData: ITransaction): Promise<object> {
         try {
-            const { portfolioId, date, currency, symbol, quantity, fees, price } = transactionData;
-
             // This is not relevant, figure out how to check for existing transaction
             // const existingTransaction = await Transaction.findOne({ transactionId })
             // if (existingTransaction) throw new BadRequestException('Transaction already exists')
 
             const transactionId = uuidv4();
+            transactionData.transactionId = transactionId
+            const newTransaction = await new Transaction(transactionData).save();
 
-            //@TODO ask Zach about this 
-            const newTransaction = await new Transaction({
-                portfolioId,
-                transactionId,
-                date,
-                currency,
-                symbol,
-                quantity,
-                fees,
-                price
-            }).save();
             return { result: { transactionId: newTransaction['transactionId'] } }
-
-
-            // const transaction = await new Transaction(transactionData).save();
-            // return { result: { transactionId: transaction['transactionId'] } };
         } catch (err) {
             console.log('err', err)
             throw err
