b1b96328af5e582b7a082db05d6b3b43bd668041
e.plougoulm <emmanuel.plougoulm@gmail.com>
2023-11-28 19:04:26 +0100

    [MM-WEB]: update Portfolio component

--- apps/web/components/ui/portfolio/portfolio.vue
+++ apps/web/components/ui/portfolio/portfolio.vue
@@ -1,7 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CardTransaction from '@/components/ui/card-transaction/card-transaction.vue';
+
+defineProps({
+  portfolio: {
+    type: Object,
+    required: true
+  }
+});
+</script>
 
 <template>
-  <div class="overview">Portfolio component</div>
+  <div class="overview">
+    <div class="name">NAME: Display a name here</div>
+    <div class="value">VALUE: Display total value here</div>
+    <div class="fiat-invested">FIAT INVESTED SINCE BEGINNING: Display FIAT here</div>
+    <div class="daily-profit-loss">Daily P/L: Display P/L here</div>
+
+    <div class="transaction">
+      <CardTransaction />
+    </div>
+
+    <div class="assets-list">Assets list: Display assets list here</div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.transaction {
+  position: absolute;
+  right: 20px;
+  width: 400px;
+}
+</style>
