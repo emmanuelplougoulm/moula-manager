fea8bac8108d7bdf9a5e7e79b1939e267ef3448a
e.plougoulm <emmanuel.plougoulm@gmail.com>
2023-11-28 18:59:15 +0100

    [MM-API]: add portfolioStore

--- apps/web/stores/portfolioStore.ts
+++ apps/web/stores/portfolioStore.ts
@@ -1,6 +1,19 @@
-export const usePortfolioStore = defineStore('wallet', {
-    state: () => ({
-        name: '',
+import type { IPortfolio } from '@/types/index';
 
-    })
+export const usePortfolioStore = defineStore('portfolio', {
+    state: () => ({
+        all: [] as IPortfolio[],
+        active: {} as IPortfolio,
+    }),
+    actions: {
+        async setPortfolios(array: Array<object>) {
+            this.all = array;
+        },
+        async setActivePortfolio(portfolio: object) {
+            this.active = portfolio;
+        },
+        isActive(id: string): boolean {
+            return this.active.portfolioId === id;
+        }
+    }
 })
