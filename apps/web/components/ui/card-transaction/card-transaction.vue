9a5667c7a61c949ea5b3fac73bf487dbe1115ce1
e.plougoulm <emmanuel.plougoulm@gmail.com>
2023-12-13 16:32:50 +0100

    [MM-WEB]: refact: <CardTransaction/>

--- apps/web/components/ui/card-transaction/card-transaction.vue
+++ apps/web/components/ui/card-transaction/card-transaction.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { storeToRefs } from 'pinia';
-import { onMounted } from 'vue';
+import { useTransactionStore } from '@/stores/transactionStore';
 
 import TabGroup from '@/components/commons/tab-group/tab-group.vue';
 import BaseInput from '@/components/commons/base-input/base-input.vue';
-import Dropdown from '@/components/commons/dropdown/dropdown.vue';
+import SearchableDropdown from '@/components/commons/searchable-dropdown/searchable-dropdown.vue';
+
 import allCoins from '@/constants/allCoins.json';
 
-import { useTransactionStore } from '@/stores/transactionStore';
+defineProps({
+  isOpen: {
+    type: Boolean,
+    required: true
+  }
+});
 
 const transactionStore = useTransactionStore();
 
@@ -17,99 +24,68 @@ const { type, portfolioId, transactionId, date, currency, asset, quantity, price
 const optionsCurrency = ['EUR', 'DOL'];
 const optionsType = ['BUY', 'SELL'];
 
-let optionsCoins: Array<object> = [];
-
-onMounted(() => {
-  optionsCoins = allCoins.map((coin) => {
-    return { value: coin.symbol, text: coin.name };
+let optionsCoins = computed(() => {
+  return allCoins.map((coin) => {
+    return { value: coin.symbol, name: coin.name };
   });
-  console.log('optionsCoins', optionsCoins);
 });
+
+// console.log('optionsCoins', optionsCoins.value);
 </script>
 
 <template>
-  <div class="grid-container">
-    <TabGroup class="grid-item" v-model="currency" :options="optionsCurrency" />
-    <TabGroup class="grid-item" v-model="type" :options="optionsType" />
+  <div
+    v-if="optionsCoins.length > 0"
+    class="wrapper-card-transaction"
+    :class="{ collapsed: isOpen }"
+  >
+    <div class="grid-container">
+      <TabGroup class="grid-item" v-model="currency" :options="optionsCurrency" />
+      <TabGroup class="grid-item" v-model="type" :options="optionsType" />
 
-    <div class="select-coin grid-item">
-      <Dropdown v-model="asset" :options="optionsCoins" />
-    </div>
-    <div class="grid-item input-group">
-      <BaseInput v-model="quantity" :label="'Quantity'" />
-      <BaseInput v-model="price" :label="'Price'" />
-    </div>
-    <div class="grid-item input-group">
-      <BaseInput v-model="date" :label="'Date'" />
-      <BaseInput v-model="fees" :label="'Fees'" />
+      <div class="grid-item">
+        <SearchableDropdown v-model="asset" :options="optionsCoins" />
+      </div>
+      <div class="grid-item">
+        <BaseInput v-model="quantity" class="input-item" :label="'Quantity'" />
+      </div>
+      <div class="grid-item">
+        <BaseInput v-model="price" class="input-item" :label="'Price'" />
+      </div>
+      <div class="grid-item">
+        <BaseInput v-model="date" :label="'Date'" />
+      </div>
+      <div class="grid-item">
+        <BaseInput v-model="fees" :label="'Fees'" />
+      </div>
+      <!-- <div>TODO CALCULATE TOTAL</div> -->
+      <button @click="handleAddTransaction">Validate</button>
     </div>
   </div>
 </template>
 
 <style scoped>
+.wrapper-card-transaction {
+  display: flex;
+  width: 350px;
+  height: 100%;
+  border: 1px var(--color-border-grey) solid;
+  padding: 2.5rem 0.8rem 0.8rem 0.8rem;
+  transform: translateX(0);
+  transition: transform 0.45s cubic-bezier(0.82, 0.085, 0.395, 0.895),
+    width 0.75s cubic-bezier(0.82, 0.085, 0.395, 0.895);
+}
+
+.wrapper-card-transaction.collapsed {
+  transform: translateX(100%);
+  width: 0;
+}
+
 .grid-container {
   display: grid;
-  grid-template-rows: 30px 30px 40px repeat(2, 70px);
+  grid-template-rows: 30px 30px 40px repeat(4, 70px);
   gap: 0.5rem;
   width: 100%;
-}
-
-.grid-item {
-  /* border: 1px solid black; */
-  /* padding: 20px; */
-  /* text-align: center; */
-}
-.input-group {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  gap: 8px;
-}
-
-.card-transaction {
-  border: 3px blue solid;
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  border-radius: var(--card-border-radius);
-  background-color: var(--color-bg-darker);
-  border: 1px var(--color-border-grey) solid;
-}
-
-.select-coin {
-  width: 100%;
-  border-radius: 4px;
-}
-.select-coin select {
-  width: 100%;
-  height: 50px;
-  background-color: var(--color-bg-lighter);
-  border: none;
-  color: white;
-}
-
-.quantity-and-price,
-.date-and-fees {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  gap: 8px;
-}
-
-.quantity,
-.price,
-.date,
-.fees {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-
-.total-spent {
-  /* width: 100%; */
-  display: flex;
-  justify-content: center;
-  height: 80px;
-  background-color: lightskyblue;
+  height: 100%;
 }
 </style>
