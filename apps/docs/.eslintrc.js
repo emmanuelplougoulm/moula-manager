80c34ccc86e0919dc65fedd61af0739232942cb2
e.plougoulm <emmanuel.plougoulm@gmail.com>
2023-12-13 13:58:43 +0100

    [MM-ALL]: remove legacy ui lib

--- apps/docs/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['custom', 'plugin:storybook/recommended'],
-  rules: {
-    'storybook/no-uninstalled-addons': ['warn', { packageJsonLocation: './apps/docs/package.json' }]
-  }
-};
