8569ced13e9e9b4ff13dece2c9ee494be56f641f
e.plougoulm <emmanuel.plougoulm@gmail.com>
2023-12-15 15:24:17 +0100

    [MM-WEB]: update card-transaction logic

--- apps/web/components/ui/card-transaction/card-transaction.vue
+++ apps/web/components/ui/card-transaction/card-transaction.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { storeToRefs } from 'pinia';
-import { useTransactionStore } from '@/stores/transactionStore';
+import { computed, reactive } from 'vue';
 import { usePortfolioStore } from '@/stores/portfolioStore';
 import { createTransaction } from '@/services/transactions/transactions.service';
 import { getPortfolioById } from '@/services/portfolios/portfolios.service';
@@ -9,6 +7,7 @@ import { getPortfolioById } from '@/services/portfolios/portfolios.service';
 import TabGroup from '@/components/commons/tab-group/tab-group.vue';
 import BaseInput from '@/components/commons/base-input/base-input.vue';
 import SearchableDropdown from '@/components/commons/searchable-dropdown/searchable-dropdown.vue';
+import Button from '@/components/commons/button/button.vue';
 
 import allCoins from '@/constants/allCoins.json';
 import { CONSTANTS } from '@/constants';
@@ -20,33 +19,42 @@ defineProps({
   }
 });
 
-const transactionStore = useTransactionStore();
-const portfolioStore = usePortfolioStore();
-
-const activePortfolioId = computed(() => portfolioStore.getActivePortfolioId);
-const { type, date, currency, asset, quantity, price, fees } = storeToRefs(transactionStore);
-
 const { FIAT_TYPES, TRANSACTION_TYPES } = CONSTANTS;
 
-const handleAddTransaction = async () => {
-  const newTransaction = {
-    type: transactionStore.type,
-    portfolioId: activePortfolioId.value,
-    date: transactionStore.date,
-    currency: transactionStore.currency,
-    asset: transactionStore.asset.value,
-    amount: transactionStore.quantity,
-    price: transactionStore.price,
-    fees: transactionStore.fees
-  };
+const emit = defineEmits(['collapse']);
+const portfolioStore = usePortfolioStore();
+const activePortfolioId = computed(() => portfolioStore.getActivePortfolioId);
 
+function getInitialState() {
+  return {
+    type: '',
+    date: '',
+    currency: '',
+    asset: {} as { [key: string]: unknown },
+    quantity: '',
+    price: '',
+    fees: ''
+  };
+}
+
+let state = reactive(getInitialState());
+
+const handleNewTransaction = async () => {
+  const newTransaction = {
+    ...state,
+    portfolioId: activePortfolioId.value,
+    asset: state.asset.value
+  };
   const response = await createTransaction(newTransaction);
 
   if (response.transactionId) {
     const response = await getPortfolioById(activePortfolioId.value);
     portfolioStore.setActivePortfolio(response);
+
+    state = reactive(getInitialState());
+    emit('collapse');
   } else {
-    // toast.add({ title: 'Error' });
+    console.log('error with transaction');
   }
 };
 
@@ -61,29 +69,19 @@ let optionsCoins = computed(() => {
   <div
     v-if="optionsCoins.length > 0"
     class="wrapper-card-transaction"
-    :class="{ collapsed: isOpen }"
+    :class="{ collapsed: !isOpen }"
   >
+    <Button :text="'< Collapse'" @click="emit('collapse')" />
     <div class="grid-container">
-      <TabGroup class="grid-item" v-model="currency" :options="FIAT_TYPES" />
-      <TabGroup class="grid-item" v-model="type" :options="TRANSACTION_TYPES" />
-
-      <div>
-        <SearchableDropdown v-model="asset" :options="optionsCoins" />
-      </div>
-      <div>
-        <BaseInput v-model="quantity" class="input-item" :label="'Quantity'" />
-      </div>
-      <div>
-        <BaseInput v-model="price" class="input-item" :label="'Price'" />
-      </div>
-      <div>
-        <BaseInput v-model="date" :label="'Date'" />
-      </div>
-      <div>
-        <BaseInput v-model="fees" :label="'Fees'" />
-      </div>
+      <TabGroup class="grid-item" v-model="state.currency" :options="FIAT_TYPES" />
+      <TabGroup class="grid-item" v-model="state.type" :options="TRANSACTION_TYPES" />
+      <SearchableDropdown v-model="state.asset" :options="optionsCoins" />
+      <BaseInput v-model="state.quantity" class="input-item" :label="'Quantity'" />
+      <BaseInput v-model="state.price" class="input-item" :label="'Price'" />
+      <BaseInput v-model="state.date" :label="'Date'" />
+      <BaseInput v-model="state.fees" :label="'Fees'" />
       <!-- <div>TODO CALCULATE TOTAL</div> -->
-      <button @click="handleAddTransaction">Validate</button>
+      <button @click="handleNewTransaction">Validate</button>
     </div>
   </div>
 </template>
@@ -91,6 +89,7 @@ let optionsCoins = computed(() => {
 <style scoped>
 .wrapper-card-transaction {
   display: flex;
+  flex-direction: column;
   width: 350px;
   height: 100%;
   border: 1px var(--color-border-grey) solid;
@@ -111,5 +110,6 @@ let optionsCoins = computed(() => {
   gap: 0.5rem;
   width: 100%;
   height: 100%;
+  margin-top: 1rem;
 }
 </style>
